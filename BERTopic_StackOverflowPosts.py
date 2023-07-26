1672f8a44f54c5d5dd890ae0b5dca45ea2f47384
Chi Vu <chi.vu@usask.ca>
2023-07-26 10:32:18 -0600

    Refactored data preprocessing and BERTopic training

--- BERTopic_StackOverflowPosts.py
+++ BERTopic_StackOverflowPosts.py
@@ -1,35 +1,63 @@
 import pandas as pd
 import re
+import nltk
+from bs4 import BeautifulSoup
 from bertopic import BERTopic
 
-new_df = pd.read_csv('Dataset/StackOverflowPostsDataset.csv')
-new_df["merged"] = new_df[["Body", "Title", "Tags"]].apply("-".join, axis=1)
-
-new_df.to_csv('Dataset/ConcatenatedDatasetSO.csv')
-
-data = new_df.merged.values.tolist()
-
-# Remove Emails
-data = [re.sub('<[^<>]*>', '', sent) for sent in data]
-# Remove new line characters
-data = [re.sub('\s+', ' ', sent) for sent in data]
-# Remove distracting single quotes
-data = [re.sub("\'", "", sent) for sent in data]
-
-# Remove stopwords
+# Download stopwords if not already available
+nltk.download('stopwords')
 from nltk.corpus import stopwords
-stop_words = stopwords.words('english')
+from nltk.stem import PorterStemmer
 
-data = [[word for word in doc.split() if word not in stop_words] for doc in data]
-data = [" ".join(doc) for doc in data]
+def clean_text(text):
+    # Remove HTML tags
+    soup = BeautifulSoup(text, "html.parser")
+    text = soup.get_text()
 
-# Train BERTopic on data
-model = BERTopic(language="english", calculate_probabilities=True)
-topics, probabilities = model.fit_transform(data)
+    # Remove non-alphanumeric characters and convert to lowercase
+    text = re.sub(r'[^A-Za-z]', ' ', text.lower())
 
-# Get topics and their top words
-topics = model.get_topic_freq()
-topics.head()
+    # Remove extra whitespaces
+    text = re.sub(r'\s+', ' ', text).strip()
 
-# Visualize Topics
-model.visualize_topics()
+    return text
+
+def remove_stopwords_and_stem(text):
+    stop_words = set(stopwords.words('english'))
+    stemmer = PorterStemmer()
+    return " ".join(stemmer.stem(word) for word in text.split() if word not in stop_words)
+
+def train_bert_topic(data):
+    model = BERTopic(language="english", calculate_probabilities=True)
+    topics, probabilities = model.fit_transform(data)
+    return model, topics, probabilities
+
+if __name__ == "__main__":
+    # Load the data
+    try:
+        new_df = pd.read_csv('Dataset/StackOverflowPostsDataset.csv')
+        new_df["merged"] = new_df[["Body", "Title", "Tags"]].apply("-".join, axis=1)
+    except FileNotFoundError:
+        print("Dataset file not found. Please provide the correct file path.")
+        exit(1)
+
+    # Preprocess the data
+    new_df["merged"] = new_df["merged"].apply(clean_text)
+    new_df["processed"] = new_df["merged"].apply(remove_stopwords_and_stem)
+
+    # Save the preprocessed data
+    new_df.to_csv('Dataset/ConcatenatedDatasetSO.csv', index=False)
+
+    # Train BERTopic on processed data
+    data = new_df["processed"].values.tolist()
+    model, topics, probabilities = train_bert_topic(data)
+
+    # Get topics and their top words
+    topics_df = model.get_topic_freq()
+    print(topics_df.head())
+
+    # Save the BERTopic model
+    model.save("bertopic_model")
+
+    # Visualize Topics
+    model.visualize_topics()
